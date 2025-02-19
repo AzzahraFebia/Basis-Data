2ad709a4950e099cab9570c69721ee7b70f1e159
AzzahraFebia <azafebiascd@gmail.com>
2025-02-19 10:53:12 +0700

    Add files via upload

---
+++ film.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 19 Feb 2025 pada 04.36
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `film`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `favorite`
+--
+
+CREATE TABLE `favorite` (
+  `Judul` varchar(100) NOT NULL,
+  `id_film` varchar(15) NOT NULL,
+  `Genre` varchar(10) NOT NULL,
+  `Tahun_rilis` date NOT NULL,
+  `Tanggal_rilis` date NOT NULL,
+  `Sutradara` varchar(20) DEFAULT NULL,
+  `Durasi` varchar(6) DEFAULT NULL,
+  `Rating` decimal(3,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `favorite`
+--
+ALTER TABLE `favorite`
+  ADD PRIMARY KEY (`id_film`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
